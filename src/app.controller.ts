9f528c8c1b0d932034096a87725b5aa8c70f4377
IslamGoher <islam.goher@nowpay.com>
2025-04-10 14:52:02 +0200

    [SE-243] Add logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,12 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private i = 1;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log({"message": `Hello World No.${this.i++}`});
     return this.appService.getHello();
   }
 }

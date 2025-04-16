51a2a60a74c6b7f454ef7111b8d01cb6488789bf
mohamed atef <mohamedatef78>
2025-04-16 09:45:52 +0200

    rename fun to processAccountingEntries and call submitInvoiceIssuanceAccountingEntries

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,25 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { AdvanceStatusLog, AccountEntries } from './backend_db/Index';
 import { sequelize } from "./backend_db/Models/InitConnection";
+import { InvoiceIssuanceService } from './invoice-issuance/invoice-issuance.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly invoiceIssuanceService: InvoiceIssuanceService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
 
-  async insertLoanIssuanceAccountEntries() {
+  async processAccountingEntries() {
     await sequelize.transaction(async (t) => {
+      // Handle loan issuance entries
       const advances = await AdvanceStatusLog.getTodayTransferredAdvances(t);
-
-      console.log({advances});
-
+      console.log('Processing loan issuance entries:', { advances });
+      
       await Promise.all(advances.map(advance => {
         const totalRevenue = Number(advance.revenue) + Number(advance.processingFees);
         const totalRevenueWithoutVAT = Number((totalRevenue / 1.15).toFixed(2));
         advance.totalRevenueWithoutVAT = totalRevenueWithoutVAT;
         return AccountEntries.insertLoanIssuance(advance, t);
       }));
+
+      // Handle invoice issuance entries
+      console.log('Start invoice issuance entries >>>>>>>>' , new Date());
+      
+      await this.invoiceIssuanceService.submitInvoiceIssuanceAccountingEntries(t);
+      console.log('Finish invoice issuance entries >>>>>>' , new Date());
+      
     });
   }
 }
